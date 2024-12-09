5a930f7f657580b1b918cb2fddd779f3b7a93a89
James Walter O. Imperial <jameswalterimperial8@gmail.com>
2024-12-09 10:55:14 +0800

    Activity1.py

---
+++ Activity1.py
@@ -0,0 +1,6 @@
+students = [
+    {"name": "James Walter O.", "id": "BSIT-2A", "program": "Bachelor of Science in Information Technology"},]
+
+print("Welcome to the Program")
+for student in students:
+    print(f"{student['name']} {student['id']} {student['program']}")
